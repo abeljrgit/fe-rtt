dd545477323b753289c5f2a543270c858c989c09
ABEL\abelm <abelmanuellugtujr1996@gmail.com>
2022-07-09 19:09:19 +0800

    tm/mp5

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
   },
   plugins: [],
 };
