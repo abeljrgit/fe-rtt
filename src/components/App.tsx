898755ad5bd030ee4083a00135e2076a4b96d64c
ABEL\abelm <abelmanuellugtujr1996@gmail.com>
2022-07-09 08:13:53 +0800

    fe-rtt

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section>
+      <div className="min-h-screen flex justify-center items-center">
+        <h1 className="text-3xl font-bold text-blue-600">React-Tailwind-Typescript(RTT)</h1>
+      </div>
+    </section>
+  );
+}
+
+export default App;
