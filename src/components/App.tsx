e6c3eaad1fa5e5223c78a1e7ddfe6685326fbbe3
ABEL\abelm <abelmanuellugtujr1996@gmail.com>
2022-07-10 11:16:18 +0800

    fe-mentor/ch19-2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,26 @@
+import productImagePc from "../img/image-product-desktop.jpg";
+import productImageSp from "../img/image-product-mobile.jpg";
+import CartIcon from "./Svg/CartIcon";
+
 function App() {
   return (
-    <section>
-      <div className="min-h-screen flex justify-center items-center">
-        <h1 className="text-3xl font-bold text-blue-600">React-Tailwind-Typescript(RTT)</h1>
+    <section className="bg-p2 min-h-screen flex items-center justify-center">
+      <div className="flex flex-col md:flex-row w-auto font-monserrat rounded-2xl overflow-hidden">
+        <img src={productImageSp} alt="Product" className="w-full max-w-sm block md:hidden" />
+        <img src={productImagePc} alt="Product" className="w-80 max-w-sm hidden md:block" />
+        <div className="flex flex-col max-w-sm md:max-w-[300px] p-8 bg-white">
+          <span className="mb-2 text-n2 tracking-[0.3em]">PERFUME</span>
+          <h1 className="font-fraunces text-3xl mb-4">Gabrielle Essence Eau De Parfum</h1>
+          <p className="mb-10 text-n2 text-sm leading-6">A floral,solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
+          <div className="flex items-center mb-4">
+            <span className="font-fraunces text-p1 text-3xl">$149.99</span>
+            <span className="ml-6 line-through text-n2">$169.99</span>
+          </div>
+          <button className="group flex items-center justify-center bg-p1 py-2.5 w-full mt-auto rounded-lg hover:bg-n1 transition duration-150 ease-linear">
+            <CartIcon />
+            <span className="text-n3 ml-4 pt-0.5">Add to Cart</span>
+          </button>
+        </div>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monserrat: ["Montserrat", "sans-serif"],
+        fraunces: ["Fraunces", "serif"],
+      },
+      colors: {
+        p1: "hsl(158, 36%, 37%)",
+        p2: "hsl(30, 38%, 92%)",
+        n1: "hsl(212, 21%, 14%)",
+        n2: " hsl(228, 12%, 48%)",
+        n3: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
